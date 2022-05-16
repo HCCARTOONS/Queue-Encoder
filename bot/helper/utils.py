e88d63b3acb37b357a9c2ed4e42f5c16e32eacab
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-16 20:14:09 +0530

    Update utils.py

--- bot/helper/utils.py
+++ bot/helper/utils.py
@@ -5,28 +5,28 @@ from pyrogram.types import Message
 from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified
 from .ffmpeg_utils import encode, get_thumbnail
 
-def on_task_complete():
+async def on_task_complete():
     del data[0]
     if len(data) > 0:
-      add_task(data[0])
+      await add_task(data[0])
 
-def add_task(message: Message):
+async def add_task(message: Message):
     try:
-      msg = message.reply_text("⬇️ **Downloading Video** ⬇️", quote=True)
-      filepath = message.download(file_name=download_dir)
-      msg.edit(f"**Encoding The Given File\n-->** ```{filepath}```")
-      new_file, og = encode(filepath)
+      msg = await message.reply_text("⬇️ **Downloading Video** ⬇️", quote=True)
+      filepath = await message.download(file_name=download_dir)
+      await msg.edit(f"**Encoding The Given File\n-->** ```{filepath}```")
+      new_file, og = await encode(filepath)
       if new_file:
-        msg.edit("**⬆️ Video Encoded Starting To Upload ⬆️**")
-        msg.edit("**⬆️ Uploading Video ⬆️**")
-        message.reply_document(new_file, file_name=og, quote=True, force_document=True, caption=og)
+        await msg.edit("**⬆️ Video Encoded Starting To Upload ⬆️**")
+        await msg.edit("**⬆️ Uploading Video ⬆️**")
+        await message.reply_document(new_file, file_name=og, quote=True, force_document=True, caption=og)
         os.remove(new_file)
-        msg.edit("**File Encoded**")
+        await msg.edit("**File Encoded**")
       else:
-        msg.edit("**Error Contact @NIRUSAKIMARVALE**")
+        await msg.edit("**Error Contact @NIRUSAKIMARVALE**")
         os.remove(filepath)
     except MessageNotModified:
       pass
     except Exception as e:
-      msg.edit(f"```{e}```")
-    on_task_complete()
+      await msg.edit(f"```{e}```")
+    await on_task_complete()
